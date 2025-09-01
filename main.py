0ef1f0c45fed3b67893ee6351bbb0af199d025ad
k0rte5 <118124741+k0rte5@users.noreply.github.com>
2025-09-01 23:14:23 +0300

    testing my memory and libs

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import requests
+
+class test_subject:
+    def __init__(self):
+        pass
+    def test_method(self):
+        print("это тестовый метод.")
+
+
+test_subject().test_method()
