adde7a695f3d9fb55a8b813a41a688abccf0f960
k0rte5 <kiselev.empl@gmail.com>
2025-09-03 01:43:08 +0300

    conversion complete

--- convert.py
+++ convert.py
@@ -2,6 +2,9 @@
 # Importing libraries
 import pandas as pd
 
-# Set the variables for reading the Excel file
-filename= 'ИХТиПЭ-очно-2курс.xlsx'           # Filename for the downloaded Excel file
-cells_to_read = 'B16:N117'                        # Columns to read
+# Set the variables for reading the Excel file, can be changed as needed
+filename= 'ИХТиПЭ-очно-2курс.xlsx'                # Filename for the downloaded Excel file
+student_group = 'ХПУ-124'                         # Student group which is also a sheet name in the Excel file
+
+# Read the Excel file
+df = pd.read_excel(filename, sheet_name=student_group, usecols='B:N', skiprows=15, nrows=102) # Schedule is always in range B16:N117 
