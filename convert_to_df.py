906efa99b1bcc507f07aa0b593cb26ffc7c57e32
k0rte5 <kiselev.empl@gmail.com>
2025-09-03 18:25:17 +0300

    small fixes in now obsolete file

--- convert_to_df.py
+++ convert_to_df.py
@@ -8,6 +8,4 @@ filename= 'ИХТиПЭ-очно-2курс.xlsx'                                
 student_group = 'ХПУ-124'                                                                     # Student group which is also a sheet name in the Excel file
 
 # Read the Excel file
-df = pd.read_excel(filename, sheet_name=student_group, usecols='B:N', skiprows=15, nrows=102) # Schedule is always in range B16:N117 
-
-print(df.iloc[6])  # Print the first row to verify correct reading
+df = pd.read_excel(filename, sheet_name=student_group, usecols='B:N', skiprows=15, nrows=102) # Schedule is always in range B16:N117
